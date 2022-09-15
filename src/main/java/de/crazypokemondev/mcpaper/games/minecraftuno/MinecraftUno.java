e9f4d3e5bcc2d2b894bb4d4ac255d9c39062e192
Olfi01 <flom@meyer-alpers.de>
2022-09-15 23:49:52 +0200

    Changes by ludwig

--- src/main/java/de/crazypokemondev/mcpaper/games/minecraftuno/MinecraftUno.java
+++ src/main/java/de/crazypokemondev/mcpaper/games/minecraftuno/MinecraftUno.java
@@ -3,14 +3,21 @@ package de.crazypokemondev.mcpaper.games.minecraftuno;
 import com.jeff_media.customblockdata.CustomBlockData;
 import de.crazypokemondev.mcpaper.games.minecraftuno.game.UnoGameLobby;
 import de.crazypokemondev.mcpaper.games.minecraftuno.helpers.BlockPos;
+import de.crazypokemondev.mcpaper.games.minecraftuno.helpers.ItemHelper;
+import de.crazypokemondev.mcpaper.games.minecraftuno.helpers.UnoConstants;
 import de.crazypokemondev.mcpaper.games.minecraftuno.listeners.DestroyBlockListener;
 import de.crazypokemondev.mcpaper.games.minecraftuno.listeners.PlaceBlockListener;
 import de.crazypokemondev.mcpaper.games.minecraftuno.listeners.PlayerInteractListener;
+import net.kyori.adventure.text.Component;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.janboerman.guilib.GuiLibrary;
 import xyz.janboerman.guilib.api.GuiListener;
+import xyz.janboerman.guilib.api.ItemBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,6 +46,15 @@ public final class MinecraftUno extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlaceBlockListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
         getServer().getPluginManager().registerEvents(new DestroyBlockListener(), this);
+
+        ItemStack unoDeck = ItemHelper.createUnoDeck();
+
+        ShapedRecipe recipe = new ShapedRecipe(UnoConstants.NAMESPACED_KEY, unoDeck);
+        recipe.shape("DMD", "MHM", "DMD");
+        recipe.setIngredient('D', Material.COBBLED_DEEPSLATE);
+        recipe.setIngredient('M', Material.MANGROVE_PLANKS);
+        recipe.setIngredient('H', Material.HAY_BLOCK);
+        getServer().addRecipe(recipe);
     }
 
     @Override
